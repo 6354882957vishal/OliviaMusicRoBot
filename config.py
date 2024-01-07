5be6094e21a572a71ca5d57cbaf308422e42c807
@SUMIT_KING <150422862+6354882957vishal@users.noreply.github.com>
2024-01-07 15:28:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID"2108417544))
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/84819fc115cb0eff32b2b.jpg")
 
-SESSION = getenv("SESSION", None)
+SESSION = getenv("SESSION", 1BVtsOGwBu4i6Iq4KX2iYmh_k6RVEVHd8Wh_jXDYBfq2sOx39i5TYn97MPieZMeWBJCsfNIAPR8E13TLL1KwnwMt-9HS35iiWn916CJ_zjvWr2qidzLBLxgR_jofbAqqzSeTm3kUzb9TUkom8oUzZxBkZrtVTYCgnyEziSwET6xPV5JoNbukVvcDfoCwaGSXsNTFwqt2SY7IQr8_FRuglRQGxs91BQwwWJFoQt6pvCgjuloxP8KMyWIJy5-gQMP_RV3vl0T0dai3otShNp__y8NuptVdtaw1qtN_BIxzDQ1s7_cemvfsFynMrtqv_sumaOvlRArwqJew7Svdxi0pYNg4cgke4j1U=)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tbcbotschat")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tbc_bots")
