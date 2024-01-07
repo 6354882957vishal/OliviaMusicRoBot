8c952707d6c58174438a07af3bfa721b68763085
@SUMIT_KING <150422862+6354882957vishal@users.noreply.github.com>
2024-01-07 15:06:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID"26489431))
+API_HASH = getenv("API_HASH"9a2fce85339bb79254a55368a61ab92f)
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN",6927939487:AAEDqqWSxxEsvQ5d7dtEQ7TCn3MgYL-SSIk )
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID"2108417544))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/84819fc115cb0eff32b2b.jpg")
